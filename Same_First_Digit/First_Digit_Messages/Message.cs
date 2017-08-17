611d62c03f130577435265014fa5ce728cc47b3c
ryan marsh <ryan marsh>
2017-08-17 12:21:12 -0400

    Boolean

---
+++ Same_First_Digit/First_Digit_Messages/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Same_First_Digit_Messages
+{
+    public class Message
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to my Same First Digit program where you will enter two numbers and I\nwill tell you whether or not they have the same first digit. Press enter to\ncontinue.");
+            Console.ReadLine();
+        }
+
+        public static void EnterNumber(string firstSecond)
+        {
+            Console.Write($"\nPlease enter the {firstSecond} number: ");
+        }
+
+        public static void NotANumber(string input, string firstSecond)
+        {
+            Console.Write($"\nSorry but {input} is not a number. Please enter the {firstSecond} number: ");
+        }
+
+        public static void AreTheSame(int digit)
+        {
+            Console.WriteLine($"\nCongratulations both your numbers have {digit} as their first digit!!! Press enter to\nclose the program.");
+        }
+
+        public static void AreNotTheSame(int digit1, int digit2)
+        {
+            Console.WriteLine($"\nUnfortunately your first number's first digit is {digit1} and your second number's\nfirst digit is {digit2} so your two numbers do not have the same first digit. Press\nenter to close the program.");
+        }
+    }
+}
