cecc6f2e39c58e61f8db4b4a6e44b48f87213cfb
ryan marsh <ryan marsh>
2017-08-21 19:53:16 -0400

    switch statements

---
+++ Sine_Cosine_Tangent/Sine_Cosine_Tangent/Utility.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sine_Cosine_Tangent_Messages;
+
+namespace Sine_Cosine_Tangent
+{
+    public class Utility
+    {
+        public double GetValidNumber()
+        {
+            Message.EnterNumber();
+            var isValidInput = false;
+            var input = "";
+            while (isValidInput == false)
+            {
+                input = Console.ReadLine();
+                if (CheckIfDouble(input) == false)
+                {
+                    Message.NotANumber(input);
+                }
+                else
+                {
+                    isValidInput = true;
+                }
+            }
+            return Double.Parse(input);
+        }
+
+        public bool CheckIfDouble(string input)
+        {
+            var num = 0.0;
+            return Double.TryParse(input, out num);
+        }
+
+        public string GetTrigFunction()
+        {
+            Message.EnterTrigFunction();
+            var isValidInput = false;
+            var input = "";
+            while (isValidInput == false)
+            {
+                input = Console.ReadLine();
+                if (CheckIfSinCosTan(input) == false)
+                {
+                    Message.NotATrigFunction(input);
+                }
+                else
+                {
+                    isValidInput = true;
+                }
+            }
+            return input;
+        }
+
+        public bool CheckIfSinCosTan(string input)
+        {
+            return ((input == "A") || (input == "B") || (input == "C"));
+        }
+
+        public double GetSine(double num)
+        {
+            return Math.Round(Math.Sin(num), 2);
+        }
+
+        public double GetCosine(double num)
+        {
+            return Math.Round(Math.Cos(num), 2);
+        }
+
+        public double GetTangent(double num)
+        {
+            return Math.Round(Math.Tan(num), 2);
+        }
+    }
+}
---
+++ Sine_Cosine_Tangent/Sine_Cosine_Tangent_Messages/Message.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sine_Cosine_Tangent_Messages
+{
+    public class Message
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to my Sine Cosine Tangent program where you will enter a number and thenchoose whether you want me to tell you that number's sine, cosine, or tangent.\nPress enter to contine.");
+            Console.ReadLine();
+        }
+
+        public static void EnterNumber()
+        {
+            Console.Write("Please enter a number: ");
+        }
+
+        public static void NotANumber(string input)
+        {
+            Console.Write($"\nSorry but {input} is not a number. Please input a number: ");
+        }
+
+        public static void EnterTrigFunction()
+        {
+            Console.WriteLine("\nPlease select a trigonomic function:\nA) Sine\nB) Cosine\nC) Tangent");
+        }
+
+        public static void NotATrigFunction(string input)
+        {
+            Console.WriteLine($"\nSorry but {input} is not a valid selection.\nPlease select a trigonomic function:\nA) Sine\nB) Cosine\nC) Tangent");
+        }
+
+        public static void SineOfNum(double num, double sineOfNum)
+        {
+            Console.WriteLine($"\nThe sine of {num} is {sineOfNum}\nPress enter to close the program.");
+        }
+
+        public static void CosineOfNum(double num, double cosineOfNum)
+        {
+            Console.WriteLine($"\nThe cosine of {num} is {cosineOfNum}\nPress enter to close the program.");
+        }
+
+        public static void TangentOfNum(double num, double tangentOfNum)
+        {
+            Console.WriteLine($"\nThe cosine of {num} is {tangentOfNum}\nPress enter to close the program.");
+        }
+    }
+}
