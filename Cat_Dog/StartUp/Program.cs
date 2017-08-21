4f6eb0fffc779ecdc0ceb2dd04afd2f74d4d602a
ryan marsh <ryan marsh>
2017-08-21 11:49:08 -0400

    loops

---
+++ Cat_Dog/StartUp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cat_Dog;
+using Cat_Dog_Messages;
+
+namespace StartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message.Welcome();
+            var utility = new Utility();
+            var userString = utility.GetValidString();
+            var catCount = utility.GetCount(userString, "cat");
+            var dogCount = utility.GetCount(userString, "dog");
+            Message.DisplayResult(catCount, dogCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Moose_Horse/Moose_Horse_Tests/UnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moose_Horse;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Moose_Horse_Tests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void ReturnsFalseForABCD()
+        {
+            var expected = false;
+
+            var utility = new Utility();
+            var actual = utility.CheckIfLongEnough("ABCD");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReturnsTrueForABCDE()
+        {
+            var expected = true;
+
+            var utility = new Utility();
+            var actual = utility.CheckIfLongEnough("ABCDE");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Returns3Forhadhorseadasdfhorseadshorseadfv()
+        {
+            var expected = 3;
+
+            var utility = new Utility();
+            var actual = utility.GetCount("hadhorseadasdfhorseamoosedshorseadfv", "horse");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Returns3Formooseasdfjmooseaweruhwemooseasdje3aemooseasdclkjh()
+        {
+            var expected = 4;
+
+            var utility = new Utility();
+            var actual = utility.GetCount("horsemooseasdfjmooseaweruhwemooseasdje3aemooseasdclkjh", "moose");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
