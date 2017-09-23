43492a06dc95ce44bd3d7dc4838490337f5a1f8f
ryan marsh <ryan marsh>
2017-09-22 21:28:54 -0400

    modulus operator

--- Max_Mod_N/Max_Mod_N/Utility.cs
+++ Max_Mod_N/Max_Mod_N/Utility.cs
@@ -53,27 +53,13 @@ namespace Max_Mod_N
 
         public int DetermineMorePowerful(int num1, int num2, int mod)
         {
-            if (num1 > num2)
+            if ((num1 > num2 && num1 % mod != num2 % mod) || (num2 > num1 && num2 % mod == num1 % mod))
             {
-                if ((num1 % mod) != (num2 % mod))
-                {
-                    return num1;
-                }
-                else
-                {
-                    return num2;
-                }
+                return num1;
             }
             else
             {
-                if ((num2 % mod) != (num1 % mod))
-                {
-                    return num2;
-                }
-                else
-                {
-                    return num1;
-                }
+                return num2;
             }
         }
     }
